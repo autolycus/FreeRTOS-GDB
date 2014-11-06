456fe721f08572703066cfd4da66ddbb17c3f466
Carl Allendorph <callendorph@gmail.com>
2014-11-06 12:20:31 -0800

    Updated the ListInspector and renamed it to stay consistent. Modified where the GDB commands are kept. Added a queue inspection method with filtering for different objects

--- src/FreeRTOS.py
+++ src/FreeRTOS.py
@@ -13,15 +13,17 @@ import gdb
 import pprint
 from Types import StdTypes 
 from List import ListInspector 
+from GDBCommands import ShowHandleName, ShowRegistry, ShowList
+from GDBCommands import ShowQueueInfo
     
 
 class Scheduler:
   
   def __init__(self): 
     
-    self._blocked = FRList("xSuspendedTaskList")
-    self._delayed1 = FRList("xDelayedTaskList1")
-    self._delayed2 = FRList("xDelayedTaskList2")
+    self._blocked = ListInspector("xSuspendedTaskList")
+    self._delayed1 = ListInspector("xDelayedTaskList1")
+    self._delayed2 = ListInspector("xDelayedTaskList2")
     self._readyLists = []
     readyTasksListsStr = "pxReadyTasksLists"
     readyListsSym,methodType = gdb.lookup_symbol(readyTasksListsStr)
@@ -30,7 +32,7 @@ class Scheduler:
       minIndex, maxIndex = readyLists.type.range()
       for i in range(minIndex, maxIndex+1):
         readyList = readyLists[i]
-        FRReadyList = FRList( readyList )
+        FRReadyList = ListInspector( readyList )
         self._readyLists.append(FRReadyList)
     else: 
       print("Failed to Find Symbol: %s" % readyTasksListsStr)
@@ -40,28 +42,28 @@ class Scheduler:
     self.PrintTableHeader()
     for i,rlist in enumerate(self._readyLists):
       if i == 0:
-        tasks,items = rlist.GetElements( "TCB_t", 0 )
+        items = rlist.GetElements( "TCB_t", 0 )
       else: 
-        tasks,items = rlist.GetElements( "TCB_t", 1 )
-      if ( len(tasks) > 0 ): 
-        print("Ready List {%d}: Num Tasks: %d" % (i, len(tasks)))
-        for t in tasks:           
-          self.PrintTaskFormatted(t)
+        items = rlist.GetElements( "TCB_t", 1 )
+      if ( len(items) > 0 ): 
+        print("Ready List {%d}: Num Tasks: %d" % (i, len(items)))
+        for tcb,val in items:           
+          self.PrintTaskFormatted(tcb)
 
-    btasks,items = self._blocked.GetElements("TCB_t")
-    print("Blocked List: Num Tasks: %d" % len(btasks))
-    for t in btasks:           
-      self.PrintTaskFormatted(t)
+    items = self._blocked.GetElements("TCB_t")
+    print("Blocked List: Num Tasks: %d" % len(items))
+    for tcb,val in items:           
+      self.PrintTaskFormatted(tcb)
 
-    d1tasks,items = self._delayed1.GetElements("TCB_t")
-    print("Delayed {1}: Num Tasks: %d" % len(d1tasks))
-    for i,t in enumerate(d1tasks):           
-      self.PrintTaskFormatted(t,items[i])
+    items = self._delayed1.GetElements("TCB_t")
+    print("Delayed {1}: Num Tasks: %d" % len(items))
+    for tcb,val in items:           
+      self.PrintTaskFormatted(tcb, val)
 
-    d2tasks,items = self._delayed2.GetElements("TCB_t")
-    print("Delayed {2}: Num Tasks: %d" % len(d2tasks))
-    for i,t in enumerate(d2tasks):           
-      self.PrintTaskFormatted(t,items[i])
+    items = self._delayed2.GetElements("TCB_t")
+    print("Delayed {2}: Num Tasks: %d" % len(items))
+    for tcb,val in items:           
+      self.PrintTaskFormatted(tcb, val)
 
 
   def PrintTableHeader(self):
@@ -78,36 +80,6 @@ class Scheduler:
     else:
       print("%16s %3s %4s" % (taskName, taskPriority, highWater))
 
-class ShowHandleName(gdb.Command):
-  """ Generate a print out of the handle by name 
-  """
-  def __init__(self):
-    super(ShowHandleName, self).__init__(
-      "show Handle-Name",
-      gdb.COMMAND_SUPPORT
-      )
-
-  def invoke(self, arg, from_tty):
-    argv = gdb.string_to_argv(arg)
-    if ( len(argv) != 1 ):
-      print("Invalid Argument: Requires one handle arg")
-    handle = int( argv[0], 0)
-    reg = HandleRegistry()
-    name = reg.GetName(handle)
-    print("Handle 0x%08x: %s" % (handle, name))
-
-class ShowRegistry(gdb.Command):
-  """ Generate a print out of the queue handle registry
-  """
-  def __init__(self):
-    super(ShowRegistry, self).__init__(
-      "show Handle-Registry", 
-      gdb.COMMAND_SUPPORT
-      )
-    
-  def invoke(self, arg, from_tty):
-    reg = HandleRegistry()
-    reg.PrintRegistry()
     
 class ShowTaskList(gdb.Command):
   """ Generate a print out of the current tasks and their states.
@@ -125,38 +97,9 @@ class ShowTaskList(gdb.Command):
 
 
 
-class ShowFRList(gdb.Command):
-  """ Generate a print out of the elements in a list 
-      passed to this command. User must pass a symbol that 
-      will be looked up. 
-  """
-
-  def __init__(self):
-    super(ShowFRList, self).__init__(
-      "show FRList", 
-      gdb.COMMAND_SUPPORT, 
-      gdb.COMPLETE_SYMBOL
-      )
-      
-  def invoke(self, arg, from_tty):
-    argv = gdb.string_to_argv(arg)    
-
-    CastTypeStr = None
-    if ( len(argv) > 0):
-      symbolArg = argv[0]
-    
-    if ( len(argv) > 1 ):
-      CastTypeStr = argv[1]
-      
-    listVal = FRList(symbolArg)
-    
-    elems = listVal.GetElements( CastTypeStr )
-
-    for  elem in elems :
-      print("Elem: %s" % str(elem))
-
-
 ShowRegistry()
 ShowList()
 ShowTaskList()
 ShowHandleName()
+ShowQueueInfo()
+
--- src/GDBCommands.py
+++ src/GDBCommands.py
@@ -8,6 +8,78 @@
 
 
 import gdb
+from List import ListInspector
+from Task import TaskInspector
+from HandleRegistry import HandleRegistry
+from Queue import QueueInspector, QueueMode
+
+class ShowQueueInfo(gdb.Command): 
+  """ Generate a print out of info about a particular 
+      set of queues.
+  """
+  def __init__(self): 
+    super(ShowQueueInfo, self).__init__(
+      "show Queue-Info", 
+      gdb.COMMAND_SUPPORT
+      )
+
+  def invoke(self, arg, from_tty): 
+    argv = gdb.string_to_argv(arg)
+    
+    qTypes = []     
+    if( len(argv) > 0 ):
+      for a in argv: 
+        try: 
+          qType = QueueMode.Map[a]
+          qTypes.append(qType)
+        except KeyError: 
+          print("Arg %s does not map to a Queue Type!" % a)
+    
+    reg = HandleRegistry()
+    qToShow = []
+    if ( len(qTypes) > 0 ):
+      # We will only print info about queues 
+      for qType in qTypes: 
+        qObjs = reg.FilterBy(qType)
+        qToShow.extend(qObjs)
+    else: 
+      qToShow = reg.FilterBy(None)
+
+    print("Num Queues: %d" % len(qToShow))
+    print("%20s %4s %16s %16s" % ("NAME", "CNT", "SEND", "RECEIVE") )
+    for q in qToShow:
+      self.PrintQueueInfo(q)
+
+  def PrintQueueInfo(self, q):
+    """ Print Info about the Queue 
+    """
+    sendList = q.GetTasksWaitingToSend()
+    rxList = q.GetTasksWaitingToReceive()
+    
+    #print("TxLen: %d, RxLen: %d" % (len(sendList), len(rxList)))
+
+    maxCount = max(len(sendList), len(rxList) )
+    outputFmt = "%20s %4s %16s %16s"
+    if ( maxCount == 0 ): 
+      print( outputFmt % (q.GetName(), q.GetQueueMessagesWaiting(), "", ""))
+    else: 
+      
+      for i in range(0, maxCount): 
+        txName = ""
+        if ( i < len(sendList) ):
+          tcbRef,val = sendList[i]
+          tcb=TaskInspector(tcbRef)
+          txName = tcb.GetName()
+        rxName = ""
+        if ( i < len(rxList) ):
+          tcbRef,val = rxList[i]
+          tcb = TaskInspector(tcbRef)
+          rxName = tcb.GetName()
+
+        if ( i == 0 ): 
+          print( outputFmt % (q.GetName(), q.GetQueueMessagesWaiting(), txName, rxName))
+        else: 
+          print( outputFmt % ("", "", txName, rxName))
 
 
 class ShowHandleName(gdb.Command):
@@ -41,21 +113,6 @@ class ShowRegistry(gdb.Command):
     reg = HandleRegistry()
     reg.PrintRegistry()
     
-class ShowTaskList(gdb.Command):
-  """ Generate a print out of the current tasks and their states.
-  """
-  def __init__(self):
-    super(ShowTaskList, self).__init__(
-      "show Task-List", 
-      gdb.COMMAND_SUPPORT
-      )
-
-  def invoke(self, arg, from_tty):
-    sched = Scheduler()
-    sched.ShowTaskList()
-
-
-
 
 class ShowList(gdb.Command):
   """ Generate a print out of the elements in a list 
@@ -64,7 +121,7 @@ class ShowList(gdb.Command):
   """
 
   def __init__(self):
-    super(ShowFRList, self).__init__(
+    super(ShowList, self).__init__(
       "show List-Handle", 
       gdb.COMMAND_SUPPORT, 
       gdb.COMPLETE_SYMBOL
@@ -80,7 +137,7 @@ class ShowList(gdb.Command):
     if ( len(argv) > 1 ):
       CastTypeStr = argv[1]
       
-    listVal = FRList(symbolArg)
+    listVal = ListInspector(symbolArg)
     
     elems = listVal.GetElements( CastTypeStr )
 

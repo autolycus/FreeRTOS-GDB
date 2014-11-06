1684be0887009b3e5ca55876de3c466c98682d5a
Carl Allendorph <callendorph@gmail.com>
2014-11-06 12:18:48 -0800

    Moved the Handle Registry to another file

--- src/FreeRTOS.py
+++ src/FreeRTOS.py
@@ -13,43 +13,6 @@ import gdb
 import pprint
 from Types import StdTypes 
 from List import ListInspector 
-
-
-
-class HandleRegistry: 
-  """ The FreeRTOS system can be configured with a table that 
-      associates a name with a QueueHandle_t.
-      This class can be used to access this table and 
-      label queue/mutex/semaphore/event groups 
-  """
-  def __init__(self, regSymbol = "xQueueRegistry"):
-    symbol, methodObj = gdb.lookup_symbol(regSymbol)
-    self._registry = symbol.value()
-    self._minIndex  = 0
-    self._maxIndex = 0
-    self._minIndex, self._maxIndex = self._registry.type.range()
-
-  def GetName(self, handle):
-    """ Find the string name associated with a queue 
-        handle if it exists in the registry 
-    """
-    for i in range(self._minIndex, self._maxIndex):
-      elem = self._registry[i]
-      h = elem['xHandle']
-      val = h.cast(StdTypes.uint32_t)
-      if ( handle == val ):
-        print("Found Entry for: %x" % handle)
-        name = elem['pcQueueName'].string()
-        return(name)
-
-  def PrintRegistry(self):
-    for i in range(self._minIndex, self._maxIndex):
-      elem = self._registry[i]
-      h = elem['xHandle']
-      if ( h != 0 ):
-        name = elem['pcQueueName'].string()
-        print("%d: %3s %16s" % (i, h, name))
-
     
 
 class Scheduler:
